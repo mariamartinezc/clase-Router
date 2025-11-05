0cbf1e3b26753a5d2fe2809e69866dd31c05fbef
PAAWSA02LC0900 <Duoc@PAAWSA02LC0900.duoc.acad>
2025-11-04 21:58:05 -0300

    avances programacion

---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavBarPrincipal from './components/navbar';
+
+function HomePrincipal() {
+    return (
+        <div>
+            <NavBarPrincipal />
+            <div className='row mt-4'>
+                <div className='col-md-12'>
+                    <h2>Pagina principal</h2>
+                    <p>dosaiud iousiosaiousa odiusap oidusapoidsua dosaiud iousiosaiousa odiusap oidusapoidsuadosaiud iousiosaiousa odiusap oidusapoidsuadosaiud iousiosaiousa odiusap oidusapoidsuadosaiud iousiosaiousa odiusap oidusapoidsuadosaiud iousiosaiousa odiusap oidusapoidsua</p>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default HomePrincipal;
---
+++ src/pages/products.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavbarPrincipal from './components/navbar';
+
+function ProductsPrincipal() {
+    return (
+        <div>
+            <NavbarPrincipal />
+            <div className='row'>
+                <div className='col-md-12'>
+                    <h2>Productos destacados</h2>
+                    <p>037298273 9482734 8387097383 037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383037298273 9482734 8387097383</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductsPrincipal;
