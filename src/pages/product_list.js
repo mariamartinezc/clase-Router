e7a5258517ccf15c654ac8521c4be5488067b3d1
PAAWSA02LC0922 <Duoc@PAAWSA02LC0922.duoc.acad>
2025-11-05 22:08:57 -0300

    map products

--- src/pages/product_list.js
+++ src/pages/product_list.js
@@ -1,8 +1,31 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Axios from 'axios';
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+import NavBarPrincipal from './components/navbar';
 
 function ListaProducto(){
+    const [products, setProducts]= useState([]);
+    useEffect(
+        () => {
+            Axios.get('http://localhost:3001/api/productos')
+            .then((response) => (setProducts(response.data)))
+            .catch((error) => console.log(error));
+        }
+    );
     
+    return(
+        <div>
+            <NavBarPrincipal/>
+            <h2>odisuoaids</h2>
+            <ul>
+                {
+                    products.map((products) => {
+                        return <li>{products.nombre}</li>
+                    })
+                }
+            </ul>
+        </div>
+    );
 
-}export default ListaProducto;
+}
+export default ListaProducto;
