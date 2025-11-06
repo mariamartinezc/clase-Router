9e587082d039874ffeda60929cb45e3f27c170d8
PAAWSA02LC0922 <Duoc@PAAWSA02LC0922.duoc.acad>
2025-11-05 21:31:02 -0300

    nuevos cambios

---
+++ src/pages/product_list.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import Axios from 'axios';
+import { useState,useEffect } from "react";
+
+function ListaProducto(){
+    
+
+}export default ListaProducto;
